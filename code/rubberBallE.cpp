9116534eba79e3221eaaf750c9700ecffa2daea2
marc <marc@kubo.ftmc.uam.es>
2021-03-03 23:18:30 +0100

    Chapter 3: Rubber ball example

---
+++ code/rubberBallE.cpp
@@ -0,0 +1,54 @@
+# include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char * argv[])
+{
+  /* State of the rubber ball */
+  float r[2]; /* Position (measured in metres) */
+  float v[2]; /* Velocity (in metres/second) */
+
+  float g = -9.8; /* Acceleration of gravity (in m/s^2) */
+
+  /* Integration parameters */
+  int nsteps = 10000; /* Number of time steps to calculate */
+  float dt = 0.001; /* Size of time step (in seconds) */
+  int printEverynSteps = 20;
+
+  /* Initial conditions */
+  r[0] = 0;
+  r[1] = 2;
+  v[0] = 0.5;
+  v[1] = 1;
+
+  /* Euler integration of the equations of motion */
+  for(int step = 0; step <= nsteps; ++step) {
+    /* New position */
+    r[0] = r[0] + v[0]*dt;
+    r[1] = r[1] + v[1]*dt;
+
+    /* New velocity */
+    v[1] = v[1] + g*dt;
+
+    /* Deal with collisions */
+    if(r[1] <= 0 && v[1] < 0) {
+      v[0] = 0.9*v[0];
+      v[1] = -0.8*v[1];
+    }
+
+    /* Output state */
+    if(printEverynSteps > 0
+       && step % printEverynSteps == 0) {
+
+      float m = 0.1; /* Mass */
+
+      /* Mechanical energy */
+      float E = 0.5*m*(v[0]*v[0] + v[1]*v[1]) - m*g*r[1];
+
+      cout<<step*dt<<" "<<r[0]<<" "<<r[1]<<" "<<E<<endl;
+    } //!
+  }
+  cout<<"# Simulated time: "<<nsteps*dt<<" seconds. #"<<endl;
+  return 0;
+}
